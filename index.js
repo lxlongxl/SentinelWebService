c577fbf069c1d065cb94fa76596942b0525197db
lxlongxl <lxlongxl@live.com>
2017-02-03 02:54:45 -0800

    data insertion into cassandra functionality added

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var request = require('request');
 var cassandra = require('cassandra-driver');
+var _ = require('underscore')
 
 var client = new cassandra.Client({ contactPoints: ['127.0.0.1:9042'], keyspace: 'test'});
+client.connect(function (error) {
+  if (error) {
+    console.log(error);
+  }
+});
 
 var app = express()
 
@@ -16,36 +22,57 @@ app.get('/updateDatabase', function (req, res) {
   request('https://data.cityoftacoma.org/resource/vzsr-722t.json', function(error, response, data){
     if (!error && response.statusCode == 200) {
       crimeData = data;
-      var crimes = {};
+      var crimeJson = {};
+      var crimes = [];
+      crimeJson.crimes = crimes;
+      console.log(crimeJson);
+      console.log('Parsing to find complete data');
       parsedCrimeData = JSON.parse(crimeData);
       //console.log(parsedCrimeData)
       for (var i = 0; i < parsedCrimeData.length; i++) {
         if(parsedCrimeData[i].intersection != null) {
-          crimes["Crime " + i]= parsedCrimeData[i];
+          crimeJson.crimes.push(parsedCrimeData[i]);
         }
       }
-      console.log(crimes);
-      var cleanCrimeData = JSON.stringify(crimes);
-      for (var crime in cleanCrimeData) {
-        var query = 'INSERT INTO crimeData (incident_number, crime, date, intersection, time) Values (?,?,?,?,?)';
-        var params = [ '1', crime.crime, crime.date, crime.intersection, crime.occured_on];
-      }
 
-      console.log('Attempting to seed db')
-
-      client.execute(query, params, { prepare: true }, function(error) {
+      console.log("Dropping existing table to import new data")
+      const dropQuery = 'DROP TABLE IF EXISTS TEST.CRIMEDATA ';
+      client.execute(dropQuery, { prepare: true }, function(error) {
         if (error) {
-          console.log(error)
-        } else {
-          console.log('The database has been updated.')
+          console.log(error);
         }
-      })
+      });
 
+      const createQuery = 'CREATE TABLE IF NOT EXISTS CRIMEDATA (incident_number text PRIMARY KEY, crime text, date date, intersection list<float>, time time);'
+      client.execute(createQuery, function(error) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Table created successfully');
+        }
+      });
+
+      //console.log(crimeJson);
+      _.each(crimeJson.crimes, function(crime){
+        console.log("Attempting insert entry into database");
+        const query = 'INSERT INTO test.crimeData (incident_number, crime, date, intersection, time) Values (?,?,?,?,?);';
+        const params = [crime.incident_number, crime.crime, crime.occurred_on, crime.intersection.coordinates, crime.approximate_time];
+
+        client.execute(query, params, { prepare: true }, function(error) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('The database has been updated.');
+          }
+        });
+      });
+    } else {
+      console.log(error);
     }
-  })
-  res.send('The database has been updated. :)')
+  });
+  res.send('The database has been updated. :)');
 })
 
 app.listen(3000, function (){
-  console.log('Example listening on port 3000')
+  console.log('Example listening on port 3000');
 })
